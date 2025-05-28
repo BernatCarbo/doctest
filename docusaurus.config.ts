95620f374273819a8ae019b6474d36500c8583ad
Bernat <bernatgarciacarbo@gmail.com>
2025-05-28 15:14:48 +0200

    search test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,8 +20,6 @@ const config: Config = {
   organizationName: 'bernatcarbo', // Usually your GitHub org/user name.
   projectName: 'doctest', // Usually your repo name.
 
-
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
@@ -45,6 +43,24 @@ const config: Config = {
           customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
+    ],
+  ],
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        docsRouteBasePath: '/',
+        // For Docs using Chinese, it is recomended to set:
+        // language: ["en", "zh"],
+
+        // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+        // forceIgnoreNoIndex: true,
+      }),
     ],
   ],
 
