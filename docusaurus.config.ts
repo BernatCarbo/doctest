b912a0dad96c9e51cec2711f547ffa7920d73cd0
Bernat <bernatgarciacarbo@gmail.com>
2025-05-30 15:20:35 +0200

    node

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://bernatcarbo.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/doctest/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -36,7 +36,7 @@ const config: Config = {
       'classic',
       {
         docs: {
-          routeBasePath: '/',
+          //routeBasePath: '/',
           sidebarPath: './sidebars.ts',
         },
         theme: {
