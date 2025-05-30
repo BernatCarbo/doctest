8825ee3771e9f3068abc927a64a35e7e0d0610cb
Bernat <bernatgarciacarbo@gmail.com>
2025-05-30 16:11:51 +0200

    node

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://bernatcarbo.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/doctest/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -94,7 +94,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ['bash', 'java'],
+      additionalLanguages: ['bash', 'java', 'csharp', 'python', 'kotlin', 'swift', 'javascript'],
     },
   } satisfies Preset.ThemeConfig,
 };
